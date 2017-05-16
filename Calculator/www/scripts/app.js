5793c1362d3d08fc8148c59207e293d8ec236dec
DESKTOP-FR36FUB\alinc <ciurea.alin@outlook.com>
2017-05-16 22:06:25 +0300

    fixes for calculator

--- Calculator/www/scripts/app.js
+++ Calculator/www/scripts/app.js
@@ -28,45 +28,47 @@
     const remove = document.getElementById("delete");
     const devide = document.getElementById("devide");
 
-    input.addEventListener("keyup", checkIfValid);
+    input.addEventListener("keydown click", checkIfValid);
 
-    zero.addEventListener("click", function () { input.value += 0; });
-    dot.addEventListener("click", function () { input.value += '.'; });
-    equal.addEventListener("click", calculate);
-    one.addEventListener("click", function () { input.value += 1; });
-    two.addEventListener("click", function () { input.value += 2; });
-    three.addEventListener("click", function () { input.value += 3; });
-    plus.addEventListener("click", function () { input.value += "+"; });
+    zero.addEventListener("click, keydown", function () { input.value += 0; });
+    dot.addEventListener("click keydown", function () { input.value += '.'; });
+    equal.addEventListener("click keydown", calculate);
+    one.addEventListener("click keydown", function () { input.value += 1; });
+    two.addEventListener("click keydown", function () { input.value += 2; });
+    three.addEventListener("click keydown", function () { input.value += 3; });
+    plus.addEventListener("click keydown", function () { input.value += "+"; });
 
-    four.addEventListener("click", function () { input.value += 4; });
-    five.addEventListener("click", function () { input.value += 5; });
-    six.addEventListener("click", function () { input.value += 6; });
-    minus.addEventListener("click", function () { input.value += "-"; });
+    four.addEventListener("click keydown", function () { input.value += 4; });
+    five.addEventListener("click keydown", function () { input.value += 5; });
+    six.addEventListener("click keydown", function () { input.value += 6; });
+    minus.addEventListener("click keydown", function () { input.value += "-"; });
 
-    seven.addEventListener("click", function () { input.value += 7; });
-    eight.addEventListener("click", function () { input.value += 8; });
-    nine.addEventListener("click", function () { input.value += 9; });
-    multiply.addEventListener("click", function () { input.value += "*"; });
+    seven.addEventListener("click keydown", function () { input.value += 7; });
+    eight.addEventListener("click keydown", function () { input.value += 8; });
+    nine.addEventListener("click keydown", function () { input.value += 9; });
+    multiply.addEventListener("click keydown", function () { input.value += "*"; });
 
-    CE.addEventListener("click", function () { input.value = "" });
-    C.addEventListener("click", function () { input.value = "" });
-    remove.addEventListener("click", function () { input.value = input.value.slice(0, input.value.length - 1); });
-    devide.addEventListener("click", function () { input.value += "/" });
+    CE.addEventListener("click keydown", function () { input.value = "" });
+    C.addEventListener("click keydown", function () { input.value = "" });
+    remove.addEventListener("click keydown", function () { input.value = input.value.slice(0, input.value.length - 1); });
+    devide.addEventListener("click keydown", function () { input.value += "/" });
 
+    equal.addEventListener("click keydown", calculate());
 
     function calculate() {
-
+        let result = input.value;
+        input.value = eval(result);
     }
 
+    let removeFirstChar = true;
     function checkIfValid() {
+        if (removeFirstChar) {
+            removeFirstChar = false;
+            input.value = input.value.substring(1);
+        }
         let value = input.value;
         let isValid = value.match(/[zxcvbnmlkjhgfdsaqwertyuiopZXCVBNMLKJHGFDSAQWERTYUIOP]/g);
-        if (isValid) {
-            console.log(`is invalid ${value.slice(value.length - 1)}`);
-        }
-        else {
-            console.log('isValid');
-        }
+        isValid ? console.log(`is invalid ${value.slice(value.length - 1)}`) : console.log('isValid');
     }
 
 }());
